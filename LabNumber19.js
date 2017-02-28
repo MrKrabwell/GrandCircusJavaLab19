ede3ca0acdee55c82616a4086f7fdacc4452bce6
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-27 20:07:38 -0500

    Works, for the most part.  Need improvements.

--- LabNumber19.js
+++ LabNumber19.js
@@ -2,12 +2,43 @@
  * Created by yosuk on 2/23/2017.
  */
 
-var fruit = ["Apples", "Bananas", "Kiwi"];
+// Array to hold the currently added products
+var fruit = [];
 
-fruit.push("Pear");
+// Array to hold the prices
+var prices = [];
+
+var total = 0.0;
 
 function display() {
-    for (var i; i = 0; i < fruit.length) {
-        document.write(fruit[i] + "<br>")
+    var table = document.getElementById('totalTable');
+
+    // for (var i; i = 0; i < prices.length) {
+    //     table.appendChild('<tr><td>' + fruit[i] + '</td><td>' + prices[i] + '</td>');
+    // }
+
+    for (i = 0; i < fruit.length; i++) {
+        var tr = document.createElement('TR');
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(fruit[i]));
+        tr.appendChild(td);
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(prices[i]));
+        tr.appendChild(td);
+        table.appendChild(tr);
+        total += prices[i];
     }
+    var tr = document.createElement('TR');
+    var td = document.createElement('TD');
+    td.appendChild(document.createTextNode('Total'));
+    tr.appendChild(td);
+    var td = document.createElement('TD');
+    td.appendChild(document.createTextNode(total));
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    //document.write("<p>The total is:</p> <br>total");
+    //var totalHTML = document.getElementById('total');
+    //totalHTML.innerHTML('<p>The total is:</p> <br>' + total);
+
 }
